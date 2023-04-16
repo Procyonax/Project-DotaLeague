6358c4756fdaf23779bf05e6ea38bd3c978dd326
Procyonax <deoxtw@gmail.com>
2023-04-16 17:36:49 +0100

    second commit, added SQL script to generate match/teams tables and updates match and team class

--- db/league.sql
+++ db/league.sql
@@ -3,8 +3,12 @@ DROP TABLE IF EXISTS teams;
 
 CREATE TABLE teams (
   id SERIAL PRIMARY KEY,
+  team_name VARCHAR(255)
 );
 
 CREATE TABLE matches (
   id SERIAL PRIMARY KEY,
+  result VARCHAR(255),
+  home_team_id INT NOT NULL REFERENCES teams(id),
+  away_team_id INT NOT NULL REFERENCES teams(id)
 );
--- models/match.py
+++ models/match.py
@@ -1,8 +1,9 @@
 class Match:
 
-    def __init__(self, score, team,  id = None, ):
-        self.score = score
-        self.team = team
+    def __init__(self, home_team_id, away_team_id, result, id = None):
+        self.home_team_id = home_team_id
+        self.away_team_id = away_team_id
+        self.result = result
         self.id = id
 
     
